cd5e745a17de085c16ace499799865e2b00845eb
pierdomenicoguarnieri <pierdo.guarnieri@gmail.com>
2023-05-09 12:09:14 +0200

    add: basic header styling

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -5,9 +5,29 @@ export default {
 </script>
 
 <template>
-  <h1>Header</h1>
+  <header>
+    <div class="container-fluid h-100 d-flex justify-content-between align-items-center px-5">
+      <div class="pg-logo">
+        <span>Logo</span>
+      </div>
+
+      <nav>
+        Menu
+      </nav>
+
+      <nav>
+        Social
+      </nav>
+    </div>
+  </header>
 </template>
 
 <style lang="scss" scoped>
+@use "../sass/partials/vars" as *;
 
+  header{
+    height: 80px;
+    background-color: $primary-color;
+    box-shadow: 0 5px 10px rgba($secondary-color, 0.1);
+  }
 </style>
