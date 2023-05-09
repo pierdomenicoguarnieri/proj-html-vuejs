3acdd2c6e5f1739b2562c29cec49e8e79a65cdec
pierdomenicoguarnieri <pierdo.guarnieri@gmail.com>
2023-05-09 18:17:56 +0200

    add: dynamic output of MenuItemSocial in Header

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,14 +1,24 @@
 <script>
 import MenuItem from "./partials/MenuItem.vue";
-import {navBar} from "../data/menus.js"
+import MenuItemSocial from "./partials/MenuItemSocial.vue";
+import {navBar, navBarSocial} from "../data/menus.js";
+import {store} from "../data/store.js";
 export default {
   name: "Header",
   components:{
-    MenuItem
+    MenuItem,
+    MenuItemSocial
   },
   data(){
     return{
-      navBar
+      navBar,
+      navBarSocial,
+      store
+    }
+  },
+  methods:{
+    getImage(img){
+      return new URL(img, import.meta.url).href;
     }
   }
 }
@@ -16,11 +26,11 @@ export default {
 
 <template>
   <header>
-    <div class="container-fluid h-100 d-flex justify-content-between align-items-center px-5">
+    <div class="container-fluid h-100 d-flex justify-content-between align-items-center ps-5">
       <div class="row row-cols-3 w-100 h-100">
-        <div class="col d-flex justify-content-start">
-          <div class="pg-logo">
-            <img src="" alt="">
+        <div class="col d-flex h-100 justify-content-start">
+          <div class="pg-logo h-100 w-100 py-4">
+            <img class="h-100" :src="getImage('../assets/img/logo.svg')" alt="">
           </div>
         </div>
         
@@ -33,8 +43,11 @@ export default {
         </div>
   
         <div class="col d-flex align-items-center justify-content-end">
-          <nav>
-            Social
+          <img :src="getImage('../assets/img/image-8.svg')" class="me-4" @click="store.showModal = true" alt="">
+          <nav class="pg-nav border-start ps-3">
+            <ul class="d-flex justify-content-evenly  list-unstyled w-100 h-100 m-0">
+              <MenuItemSocial v-for="(item, index) in navBarSocial" :key="index" :name="item.name" :img="item.img_path"/>
+            </ul>
           </nav>
         </div>
       </div>
@@ -43,13 +56,24 @@ export default {
 </template>
 
 <style lang="scss" scoped>
-@use "../sass/partials/vars" as *;
+@use "../scss/partials/vars" as *;
 
   header{
-    height: 80px;
+    height: 90px;
     background-color: $primary-color;
     box-shadow: 0 5px 10px rgba($secondary-color, 0.1);
     color: $secondary-color;
     font-weight: 700;
+  }
+  .pg-nav{
+    width: 30%;
+  }
+  img{
+    opacity: 100%;
+    cursor: pointer;
+    transition: all .3s;
+    &:hover{
+      opacity: 60%;
+    }
   }
 </style>
