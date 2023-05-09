edadef87723e03d2ac73459d6eaac2be666703d8
pierdomenicoguarnieri <pierdo.guarnieri@gmail.com>
2023-05-09 18:17:16 +0200

    add: MenuItemSocial.vue

---
+++ src/components/partials/MenuItemSocial.vue
@@ -0,0 +1,35 @@
+<script>
+export default {
+  name: "MenuItemSocial",
+  props: {
+    name: String,
+    img: String
+  },
+  methods:{
+    getImage(img){
+      return new URL(img, import.meta.url).href;
+    }
+  }
+}
+</script>
+
+<template>
+  <li class="text-center">
+    <a href="#">
+      <img :src="getImage(`../../assets/img/image-${img}.svg`)" :alt="name" :title="name">
+    </a>
+  </li>
+</template>
+
+<style lang="scss" scoped>
+  img{
+    width: 65%;
+    opacity: 100%;
+    transition: all .3s;
+  }
+  a:hover{
+    img{
+      opacity: 60%;
+    }
+  }
+</style>
