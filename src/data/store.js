ce03b7ce5cd5a28efc3407c17fe30e922266999a
pierdomenicoguarnieri <pierdo.guarnieri@gmail.com>
2023-05-11 01:01:58 +0200

    add: database for counter

--- src/data/store.js
+++ src/data/store.js
@@ -1,5 +1,38 @@
 import {reactive} from "vue";
 
 export const store = reactive({
-  showModal: false
+  counter:[
+    {
+      name: "clients",
+      text: "satisfied clients",
+      plus: "",
+      numberToShow: 0,
+      limit: 780,
+      interval: 5
+    },
+    {
+      name: "projects",
+      text: "our projects",
+      plus: "+",
+      numberToShow: 0,
+      limit: 20,
+      interval: 210
+    },
+    {
+      name: "coffe",
+      text: "cup of coffee",
+      plus: "",
+      numberToShow: 0,
+      limit: 10000,
+      interval: 1
+    },
+    {
+      name: "awards",
+      text: "our awards",
+      plus: "",
+      numberToShow: 0,
+      limit: 12,
+      interval: 340
+    }
+  ]
 })
