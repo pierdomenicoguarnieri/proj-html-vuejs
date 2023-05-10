1675c8df16d2bf71528a5eebe19aba060e766b37
pierdomenicoguarnieri <pierdo.guarnieri@gmail.com>
2023-05-11 01:08:41 +0200

    fix: minor fixes

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -1,5 +1,6 @@
 <script>
 import AboutUs from './partials/AboutUs.vue';
+import BackToTop from './partials/BackToTop.vue';
 import Parallax from './partials/Parallax.vue';
 import Section from './partials/Section.vue'
 export default {
@@ -7,20 +8,24 @@ export default {
   components: {
     Section,
     AboutUs,
-    Parallax
-}
+    Parallax,
+    BackToTop
+  }
 }
 </script>
 
 <template>
-  <Section smallTitle="pelicula studio" title="this is what we do"/>
-  <AboutUs/>
-  <Section smallTitle="your watchlist" title="best hits movie"/>
-  <Parallax className="img-1"/>
-  <Section smallTitle="the pericuros" title="meet the team"/>
-  <Parallax className="img-2"/>
-  <Section smallTitle="latest news" title="articles updated daily"/>
-  <Parallax className="img-3"/>
+  <main>
+    <Section smallTitle="pelicula studio" title="this is what we do"/>
+    <AboutUs/>
+    <Section smallTitle="your watchlist" title="best hits movie"/>
+    <Parallax className="img-1"/>
+    <Section smallTitle="the pericuros" title="meet the team"/>
+    <Parallax className="img-2"/>
+    <Section smallTitle="latest news" title="articles updated daily"/>
+    <Parallax className="img-3"/>
+    <BackToTop/>
+  </main>
 </template>
 
 <style lang="scss" scoped>
