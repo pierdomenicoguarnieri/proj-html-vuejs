ad13642513952e6be8ef109576b3417371a354b3
pierdomenicoguarnieri <pierdo.guarnieri@gmail.com>
2023-05-10 19:05:12 +0200

    add: CardSwiper markup and styling

--- src/components/partials/cards/CardSwiper.vue
+++ src/components/partials/cards/CardSwiper.vue
@@ -1,11 +1,54 @@
 <script>
-export default {
-  name: "CardSwiper"
-}
+import { Swiper, SwiperSlide } from "swiper/vue";
+import { cardSection4 } from "../../../data/menus.js";
+
+// Import Swiper styles
+import "swiper/css";
+import "swiper/css/effect-fade";
+import "swiper/css/navigation";
+import "swiper/css/pagination";
+import "swiper/swiper.css";
+
+// Import required modules
+import { Navigation,} from "swiper";
+import BlogPost from "../BlogPost.vue";
+
+  export default {
+    name: "CardSwiper",
+    components: {
+    Swiper,
+    SwiperSlide,
+    BlogPost, 
+    },
+    data(){
+      return{
+        cardSection4
+      }
+    },
+    setup() {
+      return {
+        modules: [Navigation],
+      };
+    },
+  };
 </script>
 
 <template>
-  
+  <swiper
+    :slidesPerView="3"
+    :spaceBetween="20"
+    :navigation="true"
+    :rewind="true"
+    :modules="modules"
+    class="cardSwiper">
+    <swiper-slide v-for="(slide, index) in cardSection4" :key="index">
+      <BlogPost
+        :title="slide.title"
+        :preview="slide.preview"
+        :image="slide.image"
+        :date="slide.date" />
+    </swiper-slide>
+  </swiper>
 </template>
 
 <style lang="scss" scoped>
