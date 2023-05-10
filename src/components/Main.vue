3db900791297ca86ef4b8c6d9fe30bf993405421
pierdomenicoguarnieri <pierdo.guarnieri@gmail.com>
2023-05-10 20:52:59 +0200

    add: Parallax component and dynamic class for background image

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -1,11 +1,13 @@
 <script>
 import AboutUs from './partials/AboutUs.vue';
+import Parallax from './partials/Parallax.vue';
 import Section from './partials/Section.vue'
 export default {
   name: "Main",
   components: {
     Section,
-    AboutUs
+    AboutUs,
+    Parallax
 }
 }
 </script>
@@ -14,8 +16,11 @@ export default {
   <Section smallTitle="pelicula studio" title="this is what we do"/>
   <AboutUs/>
   <Section smallTitle="your watchlist" title="best hits movie"/>
+  <Parallax className="img-1"/>
   <Section smallTitle="the pericuros" title="meet the team"/>
+  <Parallax className="img-2"/>
   <Section smallTitle="latest news" title="articles updated daily"/>
+  <Parallax className="img-3"/>
 </template>
 
 <style lang="scss" scoped>
---
+++ src/components/partials/Parallax.vue
@@ -0,0 +1,33 @@
+<script>
+export default {
+  name: "Parallax",
+  props:{
+    className: String
+  }
+}
+</script>
+
+<template>
+  <div class="pg-parallax" :class="className">
+
+  </div>
+</template>
+
+<style lang="scss" scoped>
+  .pg-parallax {
+    min-height: 500px;
+    background-attachment: fixed;
+    background-position: center;
+    background-repeat: no-repeat;
+    background-size: cover;
+  }
+  .img-1{
+    background-image: url("../../assets/img/Parallax-01.jpg");
+  }
+  .img-2{
+    background-image: url("../../assets/img/Parallax-02.jpg");
+  }
+  .img-3{
+    background-image: url("../../assets/img/Parallax-03.jpg");
+  }
+</style>
