dc2030ed658b75ad6ea28f27b7abe3a32126f482
pierdomenicoguarnieri <pierdo.guarnieri@gmail.com>
2023-05-10 17:01:26 +0200

    add: CardImages component

---
+++ src/components/partials/cards/CardImages.vue
@@ -0,0 +1,56 @@
+<script>
+export default {
+  name: "CardImages",
+  props:{
+    title: String,
+    text: String,
+    image: String
+  },
+  methods:{
+    getImage(img){
+      return new URL(img, import.meta.url).href;
+    }
+  }
+}
+</script>
+
+<template>
+  <div class="col mb-5 px-3">
+
+    <div class="pg-col-wrapper w-100 h-100">
+
+      <div class="pg-img-container mb-4 overflow-hidden">
+        <img :src="getImage(`../../../assets/img/${image}`)" alt="">
+      </div>
+
+      <div class="pg-text-container text-center">
+        <h5>{{ title }}</h5>
+        <p class="text-capitalize">{{ text }}</p>
+      </div>
+
+    </div>
+    
+  </div>
+</template>
+
+<style lang="scss" scoped>
+@use "../../../scss/partials/vars" as *;
+  .col{
+    .pg-col-wrapper{
+      .pg-img-container{
+        img{
+          transition: all .3s;
+        }
+        &:hover{
+        img{
+          transform: scale(1.04);
+        }
+      }
+      }
+      p{
+        text-transform: none;
+        color: $secondary-color-light;
+      }
+    }
+  }
+</style>
