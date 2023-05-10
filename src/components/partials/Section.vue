e31670bf0389e7313d5c3871bc035140aae3b466
pierdomenicoguarnieri <pierdo.guarnieri@gmail.com>
2023-05-10 17:02:34 +0200

    add: cycle of CardImages and CardSwiper with condition

--- src/components/partials/Section.vue
+++ src/components/partials/Section.vue
@@ -1,15 +1,21 @@
 <script>
 import Card from './cards/Card.vue';
-import { cardSection1 } from '../../data/menus';
+import CardImages from './cards/CardImages.vue';
+import CardSwiper from './cards/CardSwiper.vue';
+import { cardSection1, cardSection2, cardSection3 } from '../../data/menus';
 export default {
   name: "Section",
   data(){
     return{
-      cardSection1
+      cardSection1,
+      cardSection2,
+      cardSection3
     }
   },
   components: {
-    Card 
+    Card,
+    CardImages,
+    CardSwiper
   },
   props:{
     smallTitle: String,
@@ -23,12 +29,29 @@ export default {
     <span class="mb-4">{{smallTitle}}</span>
     <h2 class="mb-5">{{title}}</h2>
 
-    <div class="container">
+    <div class="container" v-if="smallTitle == 'pelicula studio'">
       <div class="row row-cols-3">
         <Card v-for="(card, index) in cardSection1" :key="index" :title="card.title" :text="card.text" :image="card.image"/>
       </div>
     </div>
 
+    <div class="container" v-if="smallTitle == 'your watchlist'">
+      <div class="row row-cols-3">
+        <CardImages v-for="(card, index) in cardSection2" :key="index" :title="card.title" :text="card.category" :image="card.image"/>
+      </div>
+    </div>
+
+    <div class="container" v-if="smallTitle == 'the pericuros'">
+      <div class="row row-cols-4">
+        <CardImages v-for="(card, index) in cardSection3" :key="index" :title="card.name" :text="card.role" :image="card.image"/>
+      </div>
+    </div>
+
+    <div class="container" v-if="smallTitle == 'latest news'">
+      <div class="row row-cols-4">
+        <CardSwiper/>
+      </div>
+    </div>
   </section>
 </template>
 
