d66f76891aad77f54187d529f23b12fe1a4d2f14
pierdomenicoguarnieri <pierdo.guarnieri@gmail.com>
2023-05-11 12:23:58 +0200

    add: ParallaxAutoSwiper markup and styling

---
+++ src/components/partials/parallax/ParallaxAutoSwiper.vue
@@ -0,0 +1,62 @@
+<script>
+  import { Swiper, SwiperSlide } from 'swiper/vue';
+  import "swiper/css";
+  import "swiper/css/pagination";
+  import "swiper/swiper.css";
+  import { Autoplay, Navigation } from 'swiper';
+  import { swiperParallaxAuto } from '../../../data/menus';
+
+  export default {
+    name: "ParallaxAutoSwiper",
+    components: {
+      Swiper,
+      SwiperSlide,
+    },
+    data(){
+      return{
+        swiperParallaxAuto
+      }
+    },
+    setup() {
+      return {
+        modules: [Autoplay, Navigation],
+      };
+    },
+    methods:{
+      getImage(img){
+        return new URL(img, import.meta.url).href;
+      }
+    }
+  };
+</script>
+
+<template>
+  <div class="container">
+    <swiper
+      :slidesPerView="5"
+      :spaceBetween="0"
+      :loop="true"
+      :autoplay="{
+      delay: 5000,
+      disableOnInteraction: false,
+      }"
+      :pagination="{
+        clickable: true,
+      }"
+      :modules="modules"
+      class="parallaxAutoSwiper">
+      <swiper-slide v-for="(image, index) in swiperParallaxAuto" :key="index" class="d-flex justify-content-center">
+        <img :src="getImage(`../../../assets/img/${image}`)" alt="">
+      </swiper-slide>
+      <swiper-slide v-for="(image, index) in swiperParallaxAuto" :key="index" class="d-flex justify-content-center">
+        <img :src="getImage(`../../../assets/img/${image}`)" alt="">
+      </swiper-slide>
+    </swiper>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+  .parallaxAutoSwiper{
+    padding: 100px 0;
+  }
+</style>
