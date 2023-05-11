ca63745e745c8a01577f312667be8f610b321094
pierdomenicoguarnieri <pierdo.guarnieri@gmail.com>
2023-05-11 16:06:57 +0200

    add: FooteTop components

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,13 +1,25 @@
 <script>
+import FooterTop from './partials/FooterTop.vue';
+
 export default {
-  name: "Footer"
+    name: "Footer",
+    components: { FooterTop }
 }
 </script>
 
 <template>
-  <h1>Footer</h1>
+  <footer>
+    <div class="container pt-5">
+      <FooterTop/>
+    </div>
+  </footer>
 </template>
 
 <style lang="scss" scoped>
+  @use "../scss/partials/vars" as *;
 
+  footer{
+    background-color: $secondary-color;
+    color: $primary-color;
+  }
 </style>
